48670a86be1954f0448954e2da9032d36466a029
Your Name <soren.skieller@gmail.com>
2019-03-27 00:33:16 +0100

    added more parallel classes

---
+++ Parallelization/ForEach/MyParallelForEach.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Parallelization.ForEach
+{
+    internal class MyParallelForEach
+    {
+        public void MyParallelForEachFunction<T>(IEnumerable<T> source, Action<T> body)
+        {
+            int numprocs = Environment.ProcessorCount;
+            int remainingWorkItems = numprocs;
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                using (var mre = new ManualResetEvent(false))
+                {
+                    for (int p = 0; p < numprocs; p++)
+                    {
+                        ThreadPool.QueueUserWorkItem(delegate
+                        {
+                            // Iterates through until there is no more work
+                            while (true)
+                            {
+                                // Get next item under a lock, and then process item
+                                T nextItem;
+                                lock (enumerator)
+                                {
+                                    if (!enumerator.MoveNext())
+                                    {
+                                        break;
+                                    }
+
+                                    nextItem = enumerator.Current;
+                                }
+
+                                body(nextItem);
+                            }
+
+                            if (Interlocked.Decrement(ref remainingWorkItems) == 0)
+                            {
+                                mre.Set();
+                            }
+                        });
+                    }
+
+                    mre.WaitOne();
+                }
+            }
+        }
+    }
+}
--- Parallelization/Program.cs
+++ Parallelization/Program.cs
@@ -1,8 +1,13 @@
 ﻿using Parallelization.DynamicPartitioning;
 using Parallelization.StaticPartitioning;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
+using Parallelization.ForEach;
 
 namespace Parallelization
 {
@@ -12,41 +17,95 @@ namespace Parallelization
         {
             const int waitTimeBetweenFunctionCalls = 1500; // ms
             const int lowerBound = 2;
-            const int upperBound = 500;
-
+            const int upperBound = 100;
+            
+            var range = Enumerable.Range(lowerBound,upperBound);
 
             var noThreadPools = new MyParallelForWithoutThreadPools();
             var threadPools = new MyParallelForWithThreadPool();
             var loadBalancer = new LoadBalancingMyParallelFor();
             var balancedLoadBalancer = new LoadBalancingBothStaticAndDynamic();
 
+            var parallelForEach = new MyParallelForEach();
+
             var before = DateTime.Now;
-            noThreadPools.MyParallelFor(lowerBound, upperBound, Console.WriteLine);
             var after = DateTime.Now;
-            Debug.WriteLine("No thread pools: Elapsed time: " + (after - before).TotalMilliseconds);
 
-            Thread.Sleep(waitTimeBetweenFunctionCalls);
+
+            //before = DateTime.Now;
+            //noThreadPools.MyParallelFor(lowerBound, upperBound, Console.WriteLine);
+            //after = DateTime.Now;
+            //Debug.WriteLine("No thread pools: Elapsed time: " + (after - before).TotalMilliseconds);
+
+            //Thread.Sleep(waitTimeBetweenFunctionCalls);
+
+            //before = DateTime.Now;
+            //threadPools.MyParallelFor(lowerBound, upperBound, Console.WriteLine);
+            //after = DateTime.Now;
+            //Debug.WriteLine("With thread pools: Elapsed time: " + (after - before).TotalMilliseconds);
+
+            //Thread.Sleep(waitTimeBetweenFunctionCalls);
+
+            //before = DateTime.Now;
+            //loadBalancer.MyParallelFor(lowerBound, upperBound, Console.WriteLine);
+            //after = DateTime.Now;
+            //Debug.WriteLine("Dynamic load balancing: Elapsed time: " + (after - before).TotalMilliseconds);
+
+            //Thread.Sleep(waitTimeBetweenFunctionCalls);
+
+            //before = DateTime.Now;
+            //balancedLoadBalancer.MyParallelFor(lowerBound, upperBound, Console.WriteLine);
+            //after = DateTime.Now;
+            //Debug.WriteLine("Balanced load balancing: Elapsed time: " + (after - before).TotalMilliseconds);
+
+            //Thread.Sleep(waitTimeBetweenFunctionCalls);
+
+            //before = DateTime.Now;
+            //parallelForEach.MyParallelForEachFunction(range, Console.WriteLine);
+            //after = DateTime.Now;
+            //Debug.WriteLine("For each: Elapsed time: " + (after - before).TotalMilliseconds);
+
+
+            var students = new List<Student>();
+            for (var i = 0; i < 100000; i++)
+            {
+                students.Add(new Student(i, i*10));
+            }
 
             before = DateTime.Now;
-            threadPools.MyParallelFor(lowerBound, upperBound, Console.WriteLine);
+            foreach (var student in students)
+            {
+                student.Sum = student.Grade * student.Weight;
+            }
             after = DateTime.Now;
-            Debug.WriteLine("With thread pools: Elapsed time: " + (after - before).TotalMilliseconds);
+            Debug.WriteLine("Students sequentially: Elapsed time: " + (after - before).TotalMilliseconds);
 
-            Thread.Sleep(waitTimeBetweenFunctionCalls);
+            Thread.Sleep(250);
+
 
             before = DateTime.Now;
-            loadBalancer.MyParallelFor(lowerBound, upperBound, Console.WriteLine);
+            Parallel.ForEach(students, student => { student.Sum = student.Grade * student.Weight; });
             after = DateTime.Now;
-            Debug.WriteLine("Dynamic load balancing: Elapsed time: " + (after - before).TotalMilliseconds);
+            Debug.WriteLine("Students parallel: Elapsed time: " + (after - before).TotalMilliseconds);
 
-            Thread.Sleep(waitTimeBetweenFunctionCalls);
 
-            before = DateTime.Now;
-            balancedLoadBalancer.MyParallelFor(lowerBound, upperBound, Console.WriteLine);
-            after = DateTime.Now;
-            Debug.WriteLine("Dynamic load balancing: Elapsed time: " + (after - before).TotalMilliseconds);
 
             Console.ReadKey();
         }
+    }
+
+    class Student
+    {
+        public Student(int grade, int weight)
+        {
+            Grade = grade;
+            Weight = weight;
+
+            Sum = 0;
+        }
+        public int Grade;
+        public int Weight;
+
+        public int Sum;
     }
 }
